d4c5ee98f51f059a5c30209e8be45f090f4c4f25
naresh0919 <157217952+naresh0919@users.noreply.github.com>
2024-01-22 14:24:28 +0530

    init

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-export const apiKey = 'place your openAI api key here';
+export const apiKey = 'https://api.openai.com/v1/audio/translations';
 // in some cases your api key maybe already expired
 // try to use a new account to create an api key
 
